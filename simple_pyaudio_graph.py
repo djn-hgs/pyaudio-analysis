01cb2e33fd6e07bb0f3820ce15fc9d57bf7818c5
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2025-02-03 20:01:34 +0000

    Making progress with understanding

--- simple_pyaudio_graph.py
+++ simple_pyaudio_graph.py
@@ -1,7 +1,7 @@
 import os
 import pyaudio
 import wave
-import numpy as np
+import numpy
 import numpy.fft as fft
 import matplotlib.pyplot as plt
 
@@ -13,25 +13,24 @@ CHUNK = 1024
 # Open an audio file for analysis and playback
 
 base_directory = os.getcwd()
-audio_file = 'Please.wav'
+audio_file = 'wavTones.com.unregistred.sin_1000Hz_-6dBFS_3s_lower.wav'
 filename = os.path.join(base_directory, audio_file)
-wf = wave.open(filename, "rb")
+wave_file = wave.open(filename, "rb")
 
 # Get meta
 
-rate = wf.getframerate()
-channels = wf.getnchannels()
-sample_width = wf.getsampwidth()
-
-print(sample_width)
+rate = wave_file.getframerate()
+channels = wave_file.getnchannels()
+sample_width = wave_file.getsampwidth()
 
 # Open a playback handle using PyAudio
 
-p = pyaudio.PyAudio()
+audio_out = pyaudio.PyAudio()
 
-audio_format = p.get_format_from_width(sample_width)
+audio_format = audio_out.get_format_from_width(sample_width)
 
-stream = p.open(format=audio_format, channels=channels, rate=rate, output=True)
+
+stream = audio_out.open(format=audio_format, channels=channels, rate=rate, output=True)
 
 # Create a set of axes to display our analysis
 
@@ -39,7 +38,11 @@ plt.ion()
 
 fig, ax = plt.subplots(nrows=1, ncols=1)
 
-ax.set_title(f'File: {audio_file}, Channels: {channels}, Rate: {rate}')
+title_text = f'File: {audio_file}, Channels: {channels}, Rate: {rate}'
+
+print(title_text)
+
+ax.set_title(title_text)
 ax.set_xlabel('Frequency (Hz)')
 ax.set_ylabel('log(Amplitude)')
 
@@ -53,7 +56,11 @@ while monitoring:
 
     # Grab a chunk of data from the audio file to analyse
 
-    data = wf.readframes(CHUNK)
+    data = wave_file.readframes(CHUNK)
+
+    if len(data) < CHUNK:
+        print('End of file reached')
+        monitoring = False
 
     # Play it (not necessary if we are only analysing)
 
@@ -61,7 +68,11 @@ while monitoring:
 
     # Push data into numpy for analysis
 
-    data_np = np.frombuffer(data, dtype=np.int16)
+    data_np = numpy.frombuffer(data, dtype=numpy.int16)
+
+    # Append 0s to correct size
+
+    data_np = numpy.pad(data_np, (0, CHUNK-data_np.size), mode='constant', constant_values=0)
 
     # Analyse using numpy (or scipy - just change at the top)
 
@@ -71,8 +82,8 @@ while monitoring:
 
     # Get amplitudes
 
-    amp = np.log(np.abs(yf))
-    max_amp = np.max(amp)
+    amp = numpy.log(numpy.abs(yf))
+    max_amp = numpy.max(amp)
 
     # Get the analysis frequencies
 
